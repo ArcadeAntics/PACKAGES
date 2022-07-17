ebfc48ab5106f64a4dc3b0cbd4d832143c468928
ArcadeAntics <nsnjsnd@gmail.com>
2022-07-17 02:44:49 -0400

    commit 2022-07-17 06:44:49

--- make.binary.R
+++ make.binary.R
@@ -1,12 +1,33 @@
+x <- Sys.getenv(c("r-release", "r-oldrel"), NA)
+if (any(i <- is.na(x))) {
+    warning(sprintf(
+        ngettext(sum(i), "environment variable %s is not defined",
+                         "environment variables %s are not defined"),
+        paste(encodeString(names(x)[i], quote = "\""), collapse = ", ")))
+    x <- x[!i]
+    if (length(x) <= 0L)
+        x <- R.home("bin")
+}
+
+
 if (isTRUE(attr(this.path::this.path(verbose = FALSE), "this.path.from.shell"))) {
+    FILE <- file.path(dirname(this.path::shFILE()), "..", "bin", "make.binary.R")
+    args <- commandArgs(trailingOnly = TRUE)
+} else {
+    FILE <- this.path::here(.. = 1, "bin", "make.binary.R")
+    args <- strsplit(readline("Packages to build binaries: "), "[, ]+")[[1L]]
+}
+FILE <- paste0("--file=", shQuote(FILE))
+args <- if (length(args)) c("--args", shQuote(args))
+
+
+for (name in x) {
     command <- paste(c(
-        shQuote(file.path(R.home("bin"), "R")),
+        shQuote(file.path(name, "R")),
         "--no-echo", "--no-restore",
-        paste0("--file=", shQuote(file.path(this.path::shFILE(), "..", "..", "bin", "make.binary.R"))),
-        if (length(args <- commandArgs(trailingOnly = TRUE)))
-            c("--args", shQuote(args))
+        FILE, args
     ), collapse = " ")
     cat("\n", command, "\n", sep = "")
     system(command)
     cat("\n")
-} else source(this.path::here(.. = 1, "bin", "make.binary.R"), environment(), echo = FALSE)
+}
