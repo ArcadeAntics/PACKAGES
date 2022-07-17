f78183e552e13a477f6a5d1a8eb7253792d55383
ArcadeAntics <nsnjsnd@gmail.com>
2022-07-17 01:49:03 -0400

    commit 2022-07-17 05:49:03

---
+++ commit.R
@@ -0,0 +1,26 @@
+local({
+    owd <- getwd()
+    if (is.null(owd))
+        stop("cannot 'chdir' as current directory is unknown")
+    on.exit(setwd(owd))
+
+
+    fun <- function(command) {
+        cat("\n", command, "\n", sep = "")
+        res <- system(command)
+        if (res) {
+            if (res == -1L)
+                stop(gettextf("'%s' could not be run", command), domain = NA)
+            else stop(gettextf("'%s' execution failed with error code %d", command, res), domain = NA)
+        }
+    }
+
+
+    git <- if (.Platform$OS.type == "windows") "cmd /c git" else "git"
+    for (branch in c("src", "bin")) {
+        setwd(this.path::here(.. = 1, branch))
+        fun(sprintf("%s add *"         , git))
+        fun(sprintf("%s commit -m %s"  , git, essentials::shEncode(paste("commit", structure(Sys.time(), tzone = "UTC")))))
+        fun(sprintf("%s push origin %s", git, branch))
+    }
+})
