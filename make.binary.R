f867f87dd6262705e204ea28b6b5444de92cd9a0
ArcadeAntics <nsnjsnd@gmail.com>
2023-10-29 20:53:20 -0400

    commit 2023-10-30 00:53:20.263545

--- make.binary.R
+++ make.binary.R
@@ -1,5 +1,6 @@
 main <- function (args = this.path::progArgs())
 {
+    # args <- this.path::progArgs(); stop("comment this out later")
     x <- Sys.getenv(c("r_release", "r_oldrel"), NA)
     if (any(i <- is.na(x))) {
         warning(sprintf(
@@ -7,8 +8,25 @@ main <- function (args = this.path::progArgs())
                              "environment variables %s are not defined"),
             paste(encodeString(names(x)[i], quote = "\""), collapse = ", ")))
         x <- x[!i]
-        if (length(x) <= 0L)
-            x <- R.home("bin")
+    }
+    if (length(x)) {
+        y <- Sys.getenv()
+        m <- regexec("(?i)^r_([[:digit:]]+)_([[:digit:]]+)$", names(y))
+        keep <- lengths(m) == 3L
+        y <- y[keep]
+        m <- m[keep]
+        keep <- !(y %in% x)
+        y <- y[keep]
+        m <- m[keep]
+        keep <- file.exists(y)
+        y <- y[keep]
+        m <- m[keep]
+        z <- regmatches(names(y), m)
+        z <- vapply(z, function(zz) as.integer(zz[-1L]), integer(2), USE.NAMES = FALSE)
+        y <- y[order(z[1L, ], z[2L, ], decreasing = TRUE)]
+        x <- c(x, y)
+    } else {
+        x <- R.home("bin")
     }
 
 
@@ -39,7 +57,7 @@ main <- function (args = this.path::progArgs())
         }
         Sys.setenv(R_DEFAULT_PACKAGES = "NULL")
         args <- c(
-            "--no-echo", "--no-restore", "--vanilla",
+            "-s", "--no-restore", "--vanilla",
             paste0("--file=", shQuote(FILE)),
             if (length(args)) c("--args", shQuote(args))
         )
@@ -55,6 +73,12 @@ main <- function (args = this.path::progArgs())
 
     apts <- shQuote(this.path::path.join(x, apt))
     unloadNamespace("this.path")
+    e <- Sys.getenv(c("R_LIBS", "R_LIBS_USER", "R_LIBS_SITE"), NA)
+    if (any(unset <- is.na(e)))
+        on.exit(Sys.unsetenv(names(which(unset))), add = TRUE)
+    if (any(set <- !unset))
+        on.exit(do.call(Sys.setenv, as.list(e[set])), add = TRUE)
+    Sys.unsetenv(names(e))
     for (apt in apts) {
         command <- paste(c(apt, args), collapse = " ")
         cat("\n", command, "\n", sep = "")
