88acdaf7a469b6fa06e6308e75d7a7a5cd860833
ArcadeAntics <nsnjsnd@gmail.com>
2024-01-31 03:35:14 -0500

    commit 2024-01-31 08:35:14.142035

--- make.binary.R
+++ make.binary.R
@@ -1,9 +1,9 @@
 main <- function (args = this.path::progArgs())
 {
-    # args <- "this.path"; stop("remove out later")
+    # args <- "this.path"; stop("remove this later")
     if (length(args) <= 0L) {
         if (interactive())
-            args <- strsplit(readline("Packages to build binaries: "), "[[:blank:]]+|[[:blank:]]*[,;][[:blank:]]*")[[1L]]
+            args <- strsplit(readline("Packages to build binaries: "), "[[:blank:]]+")[[1L]]
         else stop("must provide arguments or be in interactive mode")
         if (length(args) <= 0L)
             stop("expected at least 1 argument")
@@ -20,41 +20,38 @@ main <- function (args = this.path::progArgs())
         if (any(i <- is.na(x))) {
             warning(sprintf(
                 ngettext(sum(i), "environment variable %s is not defined",
-                    "environment variables %s are not defined"),
+                                 "environment variables %s are not defined"),
                 paste(encodeString(names(x)[i], quote = "\""), collapse = ", ")))
             x <- x[!i]
         }
         if (any(i <- !dir.exists(x))) {
             warning(sprintf(
                 ngettext(sum(i), "environment variable %s is not an existing directory",
-                    "environment variables %s are not existing directories"),
+                                 "environment variables %s are not existing directories"),
                 paste(encodeString(names(x)[i], quote = "\""), collapse = ", ")))
             x <- x[!i]
         }
-        if (length(x)) {
-            y <- Sys.getenv()
-            m <- regexec("(?i)^r_([[:digit:]]+)_([[:digit:]]+)$", names(y))
-            keep <- lengths(m) == 3L
-            y <- y[keep]
-            m <- m[keep]
-            keep <- !(y %in% x)
-            y <- y[keep]
-            m <- m[keep]
-            keep <- dir.exists(y)
-            y <- y[keep]
-            m <- m[keep]
-            z <- regmatches(names(y), m)
-            z <- vapply(z, function(zz) as.integer(zz[-1L]), integer(2), USE.NAMES = FALSE)
-            y <- y[order(z[1L, ], z[2L, ], decreasing = TRUE)]
-            x <- c(x, y)
-        } else {
-            x <- R.home("bin")
-        }
+        y <- Sys.getenv()
+        m <- regexec("(?i)^r_([[:digit:]]+)_([[:digit:]]+)$", names(y))
+        keep <- lengths(m) == 3L
+        y <- y[keep]
+        m <- m[keep]
+        keep <- !(y %in% x)
+        y <- y[keep]
+        m <- m[keep]
+        keep <- dir.exists(y)
+        y <- y[keep]
+        m <- m[keep]
+        z <- regmatches(names(y), m)
+        z <- vapply(z, function(zz) as.integer(zz[-1L]), integer(2), USE.NAMES = FALSE)
+        y <- y[order(z[1L, ], z[2L, ], decreasing = TRUE)]
+        x <- c(x, y)
+        if (length(x)) x else R.home("bin")
     }))
 
 
     R_version_pattern <- "^(([[:digit:]]+)\\.([[:digit:]]+))\\.[[:digit:]]+$"
-    R$versions <- vapply(R$bin, function(xx) {
+    R$version <- vapply(R$bin, function(xx) {
         apt <- if (.Platform$OS.type == "windows") "Rscript.exe" else "Rscript"
         apt <- this.path::path.join(xx, apt)
         args <- c(apt, "--default-packages=NULL", "--vanilla", "-e", "writeLines(format(getRversion()))")
@@ -76,7 +73,7 @@ main <- function (args = this.path::progArgs())
         }
         else NA_character_
     }, "")
-    R$major_minor <- sub(R_version_pattern, "\\1", R$versions)
+    R$major_minor <- sub(R_version_pattern, "\\1", R$version)
     R <- do.call("rbind", lapply(
         split(R, factor(R$major_minor, unique(R$major_minor))),
         function(r) {
@@ -84,32 +81,48 @@ main <- function (args = this.path::progArgs())
             if (n == 1L)
                r
             else
-                r[order(r$versions)[n], , drop = FALSE]
+                r[order(r$version)[n], , drop = FALSE]
         }
     ))
 
 
-    main.dir <- this.path::here(.. = 1)
+    owd <- getwd()
+    if (is.null(owd))
+        warning("cannot 'chdir' as current directory is unknown")
+
+
+    main_dir <- this.path::here(.. = 1)
+    tmp_dir <- this.path::path.join(main_dir, "tmp")
+    unlink(tmp_dir, recursive = TRUE, force = TRUE, expand = FALSE)
+    dir.create(tmp_dir, showWarnings = FALSE)
+    on.exit({
+        ## must change back to original directory before attempting to unlink
+        ## temporary directory
+        setwd(owd)
+        unlink(tmp_dir, recursive = TRUE, force = TRUE, expand = FALSE)
+    }, add = TRUE, after = FALSE)
+    setwd(tmp_dir)
+    main_dir <- ".."
 
 
     build_binary <- function(pkg, r) {
-        # main.dir <- this.path::here(.. = 1); pkg <- "this.path"; r <- data.frame(bin = Sys.getenv("r_oldrel"), version = "4.2.3", major_minor = "4.2"); stop("remove this later")
-        src.dir <- file.path("src", "contrib")
+        # dir.create(tmp_dir <- this.path::here(.. = 1, "tmp"), showWarnings = FALSE); setwd(tmp_dir); main_dir <- ".."; pkg <- "this.path"; r <- data.frame(bin = Sys.getenv("r_oldrel"), version = "4.2.3", major_minor = "4.2"); stop("remove this later")
+        contrib_dir <- file.path("src", "contrib")
         info <- read.dcf(
-            file.path(main.dir, src.dir, "PACKAGES"),
+            file.path(main_dir, contrib_dir, "PACKAGES"),
             fields = c("Package", "Version")
         )
         i <- match(pkg, info[, "Package"])
         if (is.na(i)) {
-            warning(sprintf("package '%s' does not exist in '/%s/PACKAGES'", pkg, src.dir))
+            warning(sprintf("package '%s' does not exist in '/%s/PACKAGES'", pkg, contrib_dir))
             return(FALSE)
         }
         pkgname <- info[[i, "Package"]]
         version <- info[[i, "Version"]]
-        tar.file <- paste0(pkgname, "_", version, ".tar.gz")
-        tar.path <- file.path(main.dir, src.dir, tar.file)
-        if (!file.exists(tar.path)) {
-            warning(sprintf("tarball '/%s/%s' was not found", src.dir, tar.file))
+        tar_file <- paste0(pkgname, "_", version, ".tar.gz")
+        tar_path <- file.path(main_dir, contrib_dir, tar_file)
+        if (!file.exists(tar_path)) {
+            warning(sprintf("tarball '/%s/%s' was not found", contrib_dir, tar_file))
             return(FALSE)
         }
 
@@ -126,15 +139,15 @@ main <- function (args = this.path::progArgs())
             warning("binary packages are not available")
             return(FALSE)
         }
-        bin.file <- paste0(pkgname, "_", version, ext)
-        bin.dir <- file.path("bin", platform, "contrib", r$major_minor)
-        bin.path <- file.path(main.dir, bin.dir)
-        dir.create(bin.path, showWarnings = FALSE, recursive = TRUE)
+        bin_file <- paste0(pkgname, "_", version, ext)
+        bin_dir <- file.path("bin", platform, "contrib", r$major_minor)
+        bin_path <- file.path(main_dir, bin_dir)
+        dir.create(bin_path, showWarnings = FALSE, recursive = TRUE)
 
 
         exdir <- tempfile("dir")
-        utils::untar(tar.path, DESCRIPTION.file <- file.path(pkgname, "DESCRIPTION"), exdir = exdir)
-        packageInfo <- read.dcf(file.path(exdir, DESCRIPTION.file))
+        utils::untar(tar_path, DESCRIPTION_file <- file.path(pkgname, "DESCRIPTION"), exdir = exdir)
+        packageInfo <- read.dcf(file.path(exdir, DESCRIPTION_file))
         unlink(exdir, recursive = TRUE, force = TRUE)
         if (nrow(packageInfo) != 1L) {
             warning("bruh wtf are you doing???")
@@ -145,46 +158,25 @@ main <- function (args = this.path::progArgs())
 
         fields <- c("Package", "Version", "Depends", "Suggests",
             "License", "Imports", "LinkingTo", "Enhances", "OS_type")
-        PACKAGES.info <- structure(packageInfo[fields], names = fields)
-        PACKAGES.info <- t(PACKAGES.info)
-
-
-        owd <- getwd()
-        if (is.null(owd)) {
-            warning("cannot 'chdir' as current directory is unknown")
-            return(FALSE)
-        }
-        on.exit(setwd(owd), add = TRUE)
-        setwd(bin.path)
-
-
-        tar.path <- c(rep("..", length(gregexpr("/", bin.dir)[[1L]]) + 1L), src.dir, tar.file)
-        tar.path <- paste(tar.path, collapse = "/")
+        packageInfo <- structure(packageInfo[fields], names = fields)
+        packageInfo <- t(packageInfo)
 
 
         failure <- TRUE
 
 
-        ## use getwd() instead of "." to get absolute file paths
-        files <- list.files(getwd(), full.names = TRUE)
+        files <- list.files(file.path(main_dir, bin_dir), full.names = TRUE)
         files <- files[startsWith(basename(files), paste0(pkgname, "_"))]
-        if (length(files) <= 0L) {
-        } else if (length(files) == 1L) {
-            backup <- tempfile("backup_", tmpdir = dirname(files), fileext = ext)
-            if (!file.rename(files, backup)) {
-                warning("unable to rename old tarball as a backup")
-                return(FALSE)
-            }
-            on.exit(if (failure) file.rename(backup, files) else file.remove(backup), add = TRUE)
-        } else {
-            warning("invalid 'files'")
-            return(FALSE)
-        }
+        files <- files[endsWith(basename(files), ext)]
+        files <- files[basename(files) != bin_file]
+        if (length(files))
+            on.exit(if (!failure) file.remove(files), add = TRUE, after = FALSE)
 
 
         command <- shQuote(file.path(r$bin, if (.Platform$OS.type == "windows") "R.exe" else "R"))
-        command <- paste(command, "CMD", "INSTALL", "--build", shQuote(tar.path))
+        command <- paste(command, "CMD", "INSTALL", "--build", shQuote(tar_path))
         cat("\n", command, "\n", sep = "")
+        # unloadNamespace("essentials"); unloadNamespace("this.path"); stop("remove this later")
         res <- system(command)
         cat("\n")
         if (res) {
@@ -196,70 +188,96 @@ main <- function (args = this.path::progArgs())
                     command, res, domain = "R-base"), domain = NA)
             return(FALSE)
         }
-        PACKAGES.file <- file.path(bin.path, "PACKAGES")
-        if (file.exists(PACKAGES.file)) {
-            text <- readLines(PACKAGES.file)
-            on.exit(if (failure) writeLines(text, PACKAGES.file), add = TRUE)
-            con <- file(PACKAGES.file, "w")
+        PACKAGES_path <- file.path(bin_path, "PACKAGES")
+        if (file.exists(PACKAGES_path)) {
+            text <- readLines(PACKAGES_path)
+            conn <- file("./PACKAGES", "w")
             tryCatch({
                 if (i <- match(paste0("Package: ", pkgname), text, 0L)) {
-                    writeLines(text[seq_len(i - 1L)], con)
-                } else if (i <- match(TRUE, startsWith(text, "Package: ") & substr(text, 10L, 1000000L) > pkgname, 0L)) {
-                    writeLines(text[seq_len(i - 1L)], con)
+                    writeLines(text[seq_len(i - 1L)], conn)
+                } else if (i <- match(TRUE, startsWith(text, "Package: ") & substring(text, 10L) > pkgname, 0L)) {
+                    writeLines(text[seq_len(i - 1L)], conn)
                 } else {
                     i <- length(text)
-                    writeLines(c(text, ""), con)
+                    writeLines(c(text, ""), conn)
                 }
-                write.dcf(PACKAGES.info, con)
+                write.dcf(packageInfo, conn, indent = 8L, width = 72L)
                 j <- which(text == "")
                 j <- j[j > i]
                 if (length(j) > 0) {
                     j <- j[[1L]]
-                    writeLines(text[j:length(text)], con)
+                    writeLines(text[j:length(text)], conn)
                 }
-                failure <- FALSE
-            }, error = function(c) {
-                close(con)
-                con <- NULL
-            }, finally = if (!is.null(con)) close(con))
+            }, finally = close(conn))
         } else {
-            on.exit(if (failure) file.remove(PACKAGES.file), add = TRUE)
-            write.dcf(PACKAGES.info, PACKAGES.file)
-            failure <- FALSE
+            write.dcf(packageInfo, "./PACKAGES", indent = 8L, width = 72L)
         }
+
+
+        rename_these <- c("PACKAGES", bin_file)
+        failure <- !all(file.rename(
+            file.path(".", rename_these),
+            file.path(bin_path, rename_these)
+        ))
         return(!failure)
     }
 
 
-    build_binaries <- function(r) {
+    build_binaries <- function(pkgs, r) {
         vapply(pkgs, build_binary, r, FUN.VALUE = NA)
     }
 
 
-    pkgs <- args
-    if ("--all" %in% pkgs)
-        pkgs <- setdiff(
-            read.dcf(
-                file.path(main.dir, "src", "contrib", "PACKAGES"),
-                fields = "Package"
-            ),
-            pkgs
-        )
+    all_pkgs <- read.dcf(
+        file.path(main_dir, "src", "contrib", "PACKAGES"),
+        fields = "Package"
+    )
+
+
+    args_sep <- "/"
+    argslist <- vector("list", sum(args == args_sep) + 1L)
+    indx <- 0L
+    while (i <- match(args_sep, args, 0L)) {
+        argslist[[indx <- indx + 1L]] <- args[seq_len(i - 1L)]
+        args <- args[-seq_len(i)]
+    }
+    argslist[[indx + 1L]] <- args
+    argslist <- argslist[lengths(argslist) >= 1L]
+    argslist <- lapply(argslist, function(args) {
+        m <- regexec("^--version=(.*)$", args)
+        keep <- (lengths(m) > 1L)
+        pkgs <- args[!keep]
+        if ("--all" %in% pkgs)
+            pkgs <- setdiff(all_pkgs, pkgs)
+        args <- args[keep]
+        m <- m[keep]
+        args <- regmatches(args, m)
+        args <- vapply(args, `[`, 2L, FUN.VALUE = "")
+        args <- strsplit(args, "[[:blank:]]+|[[:blank:]]*[,;][[:blank:]]*")
+        args <- unlist(args)
+        args <- if (!length(args) || "all" %in% args)
+            seq_len(nrow(R))
+        else which(R$major_minor %in% args)
+        list(pkgs = pkgs, R_indx = args)
+    })
 
 
     unloadNamespace("this.path")
     e <- Sys.getenv(c("R_LIBS", "R_LIBS_USER", "R_LIBS_SITE"), NA)
     if (any(unset <- is.na(e)))
-        on.exit(Sys.unsetenv(names(which(unset))), add = TRUE)
+        on.exit(Sys.unsetenv(names(which(unset))), add = TRUE, after = FALSE)
     if (any(set <- !unset))
-        on.exit(do.call(Sys.setenv, as.list(e[set])), add = TRUE)
+        on.exit(do.call(Sys.setenv, as.list(e[set])), add = TRUE, after = FALSE)
     Sys.unsetenv(names(e))
 
 
-    for (i in seq_len(nrow(R))) {
-        cat("\n", "Building binaries for R ", R$versions[i], "\n", sep = "")
-        print(tryCatch(build_binaries(R[i, , drop = TRUE]), error = identity))
-        cat("\n")
+    for (args in argslist) {
+        for (i in args$R_indx) {
+            r <- R[i, , drop = TRUE]
+            cat("\n", "Building binaries for R ", r$version, "\n", sep = "")
+            print(tryCatch(build_binaries(args$pkgs, r), error = identity))
+            cat("\n")
+        }
     }
 }
 
